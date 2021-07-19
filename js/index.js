5b741d8c3894405cb266678a2f7c8491acfed6dd
Austin Waldsmith <austin.waldsmith@gmail.com>
2021-07-18 21:33:38 -0400

    big styling update

--- js/index.js
+++ js/index.js
@@ -4,3 +4,41 @@
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // gsap.registerPlugin(TextPlugin, ScrollToPlugin, ScrollTrigger);
+
+const expandIcons = document.querySelectorAll(".expand");
+const text = document.querySelectorAll(".text");
+const plus = document.querySelectorAll(".plus");
+
+expandIcons.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    text[i].classList.toggle("reveal");
+    plus[i].classList.toggle("rotate");
+  });
+});
+
+const miniHeader = document.querySelector(".mini-header");
+let distance;
+
+const offSet = () => {
+  distance = window.pageYOffset;
+  //   console.log(window.pageYOffset);
+  return distance;
+};
+
+window.addEventListener("scroll", offSet);
+window.addEventListener("scroll", () => {
+  if (distance > 100) {
+    miniHeader.classList.add("reveal");
+  } else {
+    miniHeader.classList.remove("reveal");
+  }
+});
+
+const burger = document.querySelector("#burger");
+const mobileNav = document.querySelector(".mobile-nav");
+function toggleNav(e) {
+  mobileNav.classList.toggle("nav-active");
+}
+
+burger.onclick = toggleNav;
+mobileNav.onclick = toggleNav;
